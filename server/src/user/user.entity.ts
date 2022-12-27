bfa967b6efdc3c3b3d02378161b49664a41ef796
One-armed-boy <fanatic5500@gmail.com>
2022-12-27 16:12:44 +0900

    refactor: 유저 엔티티에 Soft Delete를 적용하기 위한 칼럼 추가

--- server/src/user/user.entity.ts
+++ server/src/user/user.entity.ts
@@ -1,13 +1,26 @@
-import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Timestamp,
+} from 'typeorm';
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    email: string;
+  @Column()
+  email: string;
 
-    @Column()
-    password: string;
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Timestamp;
+
+  @DeleteDateColumn({ nullable: true })
+  deletedAt: Timestamp;
 }
