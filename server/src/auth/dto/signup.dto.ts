13fa48a8b052f1dd81a01cc07a8eecfff7cfa60d
One-armed-boy <fanatic5500@gmail.com>
2023-01-02 14:27:38 +0900

    feat: 회원가입 dto 내 비밀번호 규칙을 strongpassword로 수정

--- server/src/auth/dto/signup.dto.ts
+++ server/src/auth/dto/signup.dto.ts
@@ -1,9 +1,9 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsStrongPassword } from 'class-validator';
 
 export class SignupDto {
   @IsEmail()
   readonly email: string;
 
-  @IsString()
+  @IsStrongPassword()
   readonly password: string;
 }
