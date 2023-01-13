dd5acf67a0e0da2168bde19edba80d7fe185a178
One-armed-boy <fanatic5500@gmail.com>
2023-01-13 19:32:15 +0900

    chore: jwt를 위한 JwtModule을 authModule에 추가

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -4,9 +4,27 @@ import { AuthService } from '@auth/auth.service';
 import { UserModule } from '@user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategy/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, PassportModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: {
+            expiresIn: configService.get<string>(
+              'JWT_ACCESS_TOKEN_EXPIRE_TIME',
+            ),
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
 })
