d9cd2cc40614c817aa4f5fc5acea8cf2a3dd528d
One-armed-boy <fanatic5500@gmail.com>
2023-01-14 19:00:56 +0900

    chore: AuthModule 내 새로운 프로바이더 바인딩

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -5,27 +5,40 @@ import { UserModule } from '@user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategy/local.strategy';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
+import { JwtRefreshStrategy, JwtStrategy } from './strategy/jwt.strategy';
+import { AccessTokenIssuer } from './token_issuers/access.issuer';
+import { RefreshTokenIssuer } from './token_issuers/refresh.issuer';
+import { AccessTokenExtractor } from './token_extractors/access.extractor';
+import { RefreshTokenExtractor } from './token_extractors/refresh.extractor';
 
 @Module({
   imports: [
     UserModule,
     PassportModule,
-    JwtModule.registerAsync({
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        return {
-          secret: configService.get<string>('JWT_SECRET'),
-          signOptions: {
-            expiresIn: configService.get<string>(
-              'JWT_ACCESS_TOKEN_EXPIRE_TIME',
-            ),
-          },
-        };
-      },
+    JwtModule.register({
+      // inject: [ConfigService],
+      // useFactory: async (configService: ConfigService) => {
+      //   return {
+      //     secret: configService.get<string>('JWT_ACCESS_TOKEN_SECRET'),
+      //     signOptions: {
+      //       expiresIn: configService.get<string>(
+      //         'JWT_ACCESS_TOKEN_EXPIRE_TIME',
+      //       ),
+      //     },
+      //   };
+      // },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+    JwtRefreshStrategy,
+    AccessTokenIssuer,
+    RefreshTokenIssuer,
+    AccessTokenExtractor,
+    RefreshTokenExtractor,
+  ],
 })
 export class AuthModule {}
