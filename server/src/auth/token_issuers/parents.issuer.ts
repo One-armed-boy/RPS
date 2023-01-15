396485568e54036af4d393d3032593122ed7f1b8
One-armed-boy <fanatic5500@gmail.com>
2023-01-15 19:34:05 +0900

    refactor: 가독성을 위한 줄바꿈 처리

--- server/src/auth/token_issuers/parents.issuer.ts
+++ server/src/auth/token_issuers/parents.issuer.ts
@@ -1,8 +1,8 @@
-import { JwtService, JwtSignOptions } from "@nestjs/jwt";
+import { JwtService, JwtSignOptions } from '@nestjs/jwt';
 
 export abstract class TokenIssuer {
   constructor(protected jwtService: JwtService) {}
-  
+
   sign(email?: string) {
     const [payload, signOptions] = this.getTokenOptions(email);
     return this.jwtService.sign(payload, signOptions);
