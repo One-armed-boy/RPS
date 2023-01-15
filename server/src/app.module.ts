32e2559d2d4e4af69d182853581dcdb38a7e910d
One-armed-boy <fanatic5500@gmail.com>
2023-01-15 19:32:18 +0900

    chore: 레디스 도입을 위한 환경변수 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,6 +2,7 @@ import { AuthModule } from '@auth/auth.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
+import { CacheModule } from './cache/cache.module';
 import { DatabaseModule } from './db/database.module';
 
 @Module({
@@ -11,12 +12,19 @@ import { DatabaseModule } from './db/database.module';
       envFilePath: '.env',
       validationSchema: Joi.object({
         DEV_MODE: Joi.boolean().default(false),
+
         DB_USERNAME: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
         DB_DATABASE: Joi.string().required(),
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.number().required(),
+
+        CACHE_PASSWORD: Joi.string().required(),
+        CACHE_HOST: Joi.string().required(),
+        CACHE_PORT: Joi.number().required(),
+
         PASSWORD_HASH_ROUND: Joi.number().required(),
+
         JWT_ACCESS_TOKEN_SECRET: Joi.string().required(),
         JWT_ACCESS_TOKEN_EXPIRE_TIME: Joi.string().required(),
         JWT_REFRESH_TOKEN_SECRET: Joi.string().required(),
@@ -24,6 +32,7 @@ import { DatabaseModule } from './db/database.module';
       }),
     }),
     DatabaseModule,
+    CacheModule,
     AuthModule,
   ],
   controllers: [],
