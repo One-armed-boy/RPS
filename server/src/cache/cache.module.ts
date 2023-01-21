a3313f07e9fb4370ff90932a85b08640d1d88cb8
One-armed-boy <fanatic5500@gmail.com>
2023-01-21 22:36:30 +0900

    refactor: 글로벌 모듈로 선언되어있던 캐시 모듈을 더 이상 글로벌하지 않도록 수정

--- server/src/cache/cache.module.ts
+++ server/src/cache/cache.module.ts
@@ -1,8 +1,7 @@
-import { Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CacheService } from './cache.service';
 import { RedisCacheModule } from './redis/redis.module';
 
-@Global()
 @Module({
   imports: [RedisCacheModule],
   providers: [CacheService],
